57b6abb91bcc77bd5c4055a383b92ce3f47d5148
Colin Watson <cojwatson@gmail.com>
2024-04-15 16:28:08 +1000

    Add smarter permutation generation

--- Program.cs
+++ Program.cs
@@ -22,30 +22,57 @@ internal class Program
             allWords.Add(word + (year - 1).ToString()[2..]);
             allWords.Add(word + year.ToString()[2..]);
 
-            // Replace s's with $'s and o's with 0's
-            allWords.Add(word.Replace('s', '$').Replace('S', '$').Replace('o', '0').Replace('O', '0'));
+            // Replace s with $, o with 0 and a with @
+            allWords.Add(word.Replace('s', '$').Replace('S', '$').Replace('o', '0').Replace('O', '0').Replace('a', '@').Replace('A', '@'));
             allWords.Add(word.Replace('s', '$').Replace('S', '$'));
             allWords.Add(word.Replace('o', '0').Replace('O', '0'));
             allWords.Add(word.Replace('a', '@').Replace('A', '@'));
 
-            // Add basic numbers
-            allWords.Add(word + "1");
-            allWords.Add(word + "2");
-            allWords.Add(word + "3");
+            // Add basic numbers (up to 12 because it's common to use months in passwords)
+            foreach(var num in Enumerable.Range(0, 12))
+            {
+                allWords.Add(word + num);
+
+                // Also include 0 padded version for single digit numbers
+                if (num < 10)
+                {
+                    allWords.Add(word + "0" + num);
+                }
+
+                // People will commonly use a number after some special chars
+                allWords.Add(word + "@" + num);
+                allWords.Add(word + "_" + num);
+                allWords.Add(word + "!" + num);
+                allWords.Add(word + "#" + num);
+            }
+
+            // Some common number combos
             allWords.Add(word + "123");
+            allWords.Add(word + "456");
+            allWords.Add(word + "420"); // Some people think they're funny
+            allWords.Add(word + "69"); // Some are
 
-            // Add exclamation marks
+            // Add special chars
             allWords.Add(word + "!");
+            allWords.Add(word + "@");
+            allWords.Add(word + "_");
+            allWords.Add(word + "#");
+        }
 
-            // Add numbers and special chars
-            allWords.Add(word + "1!");
-            allWords.Add(word + "123!");
-            allWords.Add(word + "@1");
-            allWords.Add(word + "_1");
+        // Add some extra permutations for all generated words with special chars after
+        foreach(var word in allWords.ToList())
+        {
+            allWords.Add(word + "!");
+            allWords.Add(word + "@");
+            allWords.Add(word + "_");
+            allWords.Add(word + "#");
         }
 
         // Filter invalid passwords (Less than 8 chars)
-        allWords = allWords.Where(w => w.Length >= 8).ToList();
+        allWords = allWords.Where(w => w.Length >= 8).Distinct().ToList();
+
+        // Make sure there's no double ups
+        allWords = allWords.Distinct().ToList();
 
         Console.WriteLine("Done :)");
         File.WriteAllLines("custom_wordlist.txt", allWords);
